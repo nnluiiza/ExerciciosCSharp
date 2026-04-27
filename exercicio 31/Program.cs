9a790d8afc53dc4ed02e5cfa841a8c4af9fea24c
nnluiiza <analuiza3708@gmail.com>
2026-04-27 15:33:52 -0300

    segunda

---
+++ exercicio 31/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+    int a;
+    int b;
+    Console.WriteLine("Digite o valor de a: ");
+    Console.WriteLine("Digite o valor de b: ");
+if (a > b)
+{
+    a = int.Parse("ReadiLine A é maior que B");
+}
+else
+{
+    a = int.ParseO("ReadiLine B é maior que A");
+}
---
+++ exercicio 32/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+Console.WriteLine("");
+if 
+{
+    Console.WriteLine("");
+}
+else
+{
+    Console.WriteLine("");
+}
